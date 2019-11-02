1df25f48ae9fcdc797faea903b2076e6e5d708ac
mategoncalves <mategoncalves@gmail.com>
2019-11-02 19:11:00 -0300

    foi?

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const http = require('http');
 
-const fs=require('fs');
+const app = express();
 
-fs.readFile('index.html',(err,html)=>{
-  if(err){
-    throw err;
-  }
-  const hostname = '127.0.0.1';
-  const port = 80;
+/*const fs=require('fs');
 
-  const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-type','text/html');
-    res.write(html);
-    res.end();
-  });
+const hostname = '127.0.0.1';
+const port = 80;
 
-  server.listen(port,hostname,()=>{
-    console.log('Comecou na porta '+port);
-  });
+const server = http.createServer((req,res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-type','text/plain');
+  res.write('Ata');
+  res.end();
+});
+
+server.listen(port,hostname,()=>{
+  console.log('Comecou na porta '+port);
+});*/
+
+const logador = function(req,res,next){
+  console.log('Logando');
+  next();
+}
+
+app.use(logador);
+
+app.get('/',function(req,res){
+  res.send("ola q tal");
+});
+
+app.listen(80,function(){
+  console.log('Comecou em 80');
 });
